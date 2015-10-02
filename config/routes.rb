a9171b2baac171c9a6e1530aad971751a21f4895
Evan Berg <evan.m.berg@gmail.com>
2015-10-02 13:04:57 -0400

    created sessions controller and actions

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,20 @@ Rails.application.routes.draw do
 
 
 
+  get 'sessions/new'
+
+  get 'sessions/create'
+
+  get 'sessions/destroy'
+
   # You can have the root of your site routed with "root"
   root 'welcome#index'
 
   resource :users, only: [:create, :new]
+
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+
 
 end
