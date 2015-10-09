b8dd55d203e4b5015711207733e96d0ef28a7d98
Evan Berg <evan.m.berg@gmail.com>
2015-10-09 17:53:41 -0400

    paperclip is working!! lick lick lick my balls!

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -24,18 +24,18 @@ class ImagesController < ApplicationController
   # POST /images
   # POST /images.json
   def create
-    # @image = Image.new(image_params)
-    #
-    # respond_to do |format|
-    #   if @image.save
-    #     format.html { redirect_to @image, notice: 'Image was successfully created.' }
-    #     format.json { render :show, status: :created, location: @image }
-    #   else
-    #     format.html { render :new }
-    #     format.json { render json: @image.errors, status: :unprocessable_entity }
-    #   end
-    # end
-    @image = Image.create(image_params)
+    @image = Image.new(image_params)
+
+    respond_to do |format|
+      if @image.save
+        format.html { redirect_to @image, notice: 'Image was successfully created.' }
+        format.json { render :show, status: :created, location: @image }
+      else
+        format.html { render :new }
+        format.json { render json: @image.errors, status: :unprocessable_entity }
+      end
+    end
+    # @image = Image.create(image_params)
   end
 
   # PATCH/PUT /images/1
