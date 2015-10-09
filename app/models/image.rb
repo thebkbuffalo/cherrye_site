a08e897ac7133d20f96d41829f0230a15bd26667
Evan Berg <evan.m.berg@gmail.com>
2015-10-09 17:44:07 -0400

    added model logic

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,2 +1,4 @@
 class Image < ActiveRecord::Base
+  has_attached_file :photo, styles: { medium: "300x300", thumb: "100x100" }, default_url: "http://zenmonkeystudios.com/wp-content/uploads/2015/09/tiny-rick-school-mockup02.jpg"
+  validates_attachment_content_type :photo, content_type: /\Aimage\/.*\Z/
 end
