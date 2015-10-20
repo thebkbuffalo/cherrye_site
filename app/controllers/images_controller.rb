fa905e3f1975c8e6c523887173e8894feaf6c12a
Evan Berg <evan.m.berg@gmail.com>
2015-10-20 14:26:58 -0400

    added caption to images

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -78,6 +78,6 @@ class ImagesController < ApplicationController
     # Never trust parameters from the scary internet, only allow the white list through.
     def image_params
       # params[:image]
-      params.require(:image).permit(:photo)
+      params.require(:image).permit(:photo, :caption)
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151015205827) do
+ActiveRecord::Schema.define(version: 20151020181502) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 20151015205827) do
     t.string   "photo_content_type"
     t.integer  "photo_file_size"
     t.datetime "photo_updated_at"
+    t.string   "caption"
   end
 
   create_table "performances", force: :cascade do |t|
