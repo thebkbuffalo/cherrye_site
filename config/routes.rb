8863a9c7c7314168957ab6b71e71e13d7ee8683c
Evan Berg <evan.m.berg@gmail.com>
2015-10-24 00:55:08 -0400

    added bio section, but bold and italic arent working on the text edit bar

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
 
-  resources :biographies
+  resources :biographies, :path => "bio"
   resources :playwrights, :path => "playwright"
   resources :performances
   resources :videos
