3240800835ed58f16ed9f6cff0f0ebf7f60065da
Evan Berg <evan.m.berg@gmail.com>
2015-10-24 00:31:31 -0400

    bunch of work to basic setting up of front end

--- app/helpers/videos_helper.rb
+++ app/helpers/videos_helper.rb
@@ -4,11 +4,11 @@ module VideosHelper
     # if url contains youtube run
     if url.include?("youtube")
       youtube_id = url.split('=').last
-      content_tag(:iframe, nil, src: "//www.youtube.com/embed/#{youtube_id}")
+      content_tag(:iframe, nil, src: "//www.youtube.com/embed/#{youtube_id}", width: "600px", height: "300px")
     # else contains vimeo run
     else url.include?("vimeo")
       vimeo_id = url.split('/').last
-      content_tag(:iframe, nil, src: "https://player.vimeo.com/video/#{vimeo_id}")
+      content_tag(:iframe, nil, src: "https://player.vimeo.com/video/#{vimeo_id}", width: "600px", height: "300px")
     end
   end
 end
