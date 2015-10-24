7dfa6c8b06da753ec3b40480066041f18aef7e5a
Evan Berg <evan.m.berg@gmail.com>
2015-10-23 22:14:55 -0400

    background video is working, redirect to blog after video plays, could also add a click button, yada yada yada, all the ideas and shit

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -2,4 +2,9 @@ $(document).ready(function(){
   $(function() {
     $('.edit').froalaEditor()
   });
+
+  $('#bgvid').on('ended', function(){
+    console.log("video ended");
+    window.location.assign('http://localhost:3000/blog');
+  });
 });
