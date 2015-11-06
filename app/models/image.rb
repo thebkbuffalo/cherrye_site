0ca15881c40ab4fd246c73538bb398c9496e795a
Evan Berg <evan.m.berg@gmail.com>
2015-11-05 20:14:07 -0500

    changed default image for paperclip uploads

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,4 +1,4 @@
 class Image < ActiveRecord::Base
-  has_attached_file :photo, styles: { medium: "300x300", thumb: "100x100" }, default_url: "http://zenmonkeystudios.com/wp-content/uploads/2015/09/tiny-rick-school-mockup02.jpg"
+  has_attached_file :photo, styles: { medium: "300x300", thumb: "100x100" }, default_url: "http://www.placecage.com/400/500"
   validates_attachment_content_type :photo, content_type: /\Aimage\/.*\Z/
 end
